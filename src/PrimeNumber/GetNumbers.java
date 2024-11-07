ad97a922b3dac0897bd4619f7b31e5f2ee54dd97
Ensep <trufanovwork@gmail.com>
2024-11-07 20:28:57 +0300

    create get number func

--- src/PrimeNumber/GetNumbers.java
+++ src/PrimeNumber/GetNumbers.java
@@ -1,4 +1,27 @@
 package PrimeNumber;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
 public class GetNumbers {
+    public static void getNumbers() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    int number = Integer.parseInt(line);
+                    list.add(number);
+                } catch (NumberFormatException e) {
+                    System.out.println("Введите корректное число.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
