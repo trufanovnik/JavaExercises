7a4a847b277be0f9641a85a02b8f1d3c0a663d67
Ensep <trufanovwork@gmail.com>
2024-11-07 20:31:58 +0300

    refacr getnumbers

--- src/PrimeNumber/GetNumbers.java
+++ src/PrimeNumber/GetNumbers.java
@@ -3,7 +3,6 @@ package PrimeNumber;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
 
 public class GetNumbers {
