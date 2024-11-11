01b3b1c377f95cf1ce142e4b8b27aaba113dc7bb
Ensep <trufanovwork@gmail.com>
2024-11-11 13:27:56 +0300

    complete delete number from array

--- src/DeleteFromArray/DeleteNumberFromArray.java
+++ src/DeleteFromArray/DeleteNumberFromArray.java
@@ -1,5 +1,29 @@
 package DeleteFromArray;
 
-public class DeleteNumberFromArray {
+import java.util.Arrays;
 
+public class DeleteNumberFromArray {
+    public static int[] deleteNumberFromArray(int[] array, int number){
+        int [] modifiedArray;
+        int modifiedArraySize;
+        int counter = 0;
+
+        for(int element : array){
+            if(element == number){counter++;}
+        }
+        modifiedArraySize = array.length - counter;
+        modifiedArray = new int[modifiedArraySize];
+
+        for (int i = 0, j = 0; i < modifiedArraySize;) {
+            if(array[j] != number){
+                modifiedArray[i] = array[j];
+                j++;
+                i++;
+            } else {
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(modifiedArray));
+        return modifiedArray;
+    }
 }
--- src/DeleteFromArray/Main.java
+++ src/DeleteFromArray/Main.java
@@ -1,9 +1,16 @@
 package DeleteFromArray;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int [] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] array = {0, 1, 2, 3, 4, 5, 5, 7, 5, 5, 10};
+
         GetNumber getNumber = new GetNumber();
         int number = getNumber.getNumber();
+        System.out.println("Изначальный массив: ");
+        System.out.println(Arrays.toString(array));
+        System.out.println("Измененный массив: ");
+        DeleteNumberFromArray.deleteNumberFromArray(array, number);
     }
 }
