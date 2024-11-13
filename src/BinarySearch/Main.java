317cb16eb854f2d5c6a5e2347a55e78f2374d075
Ensep <trufanovwork@gmail.com>
2024-11-13 22:49:17 +0300

    added bruteforce method and time comparison with binarysearch

--- src/BinarySearch/Main.java
+++ src/BinarySearch/Main.java
@@ -1,13 +1,26 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-        int value = 2;
-        System.out.println(binarySearch(array, value, 0, (array.length - 1)));
+        double[] array = generateArray(1000000);
+        Arrays.sort(array);
+
+        long time = System.currentTimeMillis();
+        bruteForce(array, 0.7);
+        System.out.println(System.currentTimeMillis() - time);
+
+        time = System.currentTimeMillis();
+        binarySearch(array, 0.7, 0, array.length - 1);
+        System.out.println(System.currentTimeMillis() - time);
+
+        double[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        double value = 20;
+        System.out.println(binarySearch(sortedArray, value, 0, (sortedArray.length - 1)));
     }
 
-    public static boolean binarySearch(int[] array, int value, int from, int to){
+    private static boolean binarySearch(double[] array, double value, int from, int to){
         if (from > to)
             return false;
 
@@ -20,5 +33,19 @@ public class Main {
         } else {
             return binarySearch(array, value, from, mid - 1);
         }
+    }
+    private static boolean bruteForce(double[] array, double value){
+        for (double v : array) {
+            if (v == value)
+                return true;
+        }
+        return false;
+    }
+    public static double[] generateArray(int numbers){
+        double[] array = new double[numbers];
+        for (int i=0; i < numbers; i++){
+            array[i] = Math.random();
+        }
+        return array;
     }
 }
