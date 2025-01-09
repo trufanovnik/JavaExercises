63d5e6295b534c34c76beb4cbd05968d052c4852
Ensep <trufanovwork@gmail.com>
2025-01-09 19:04:11 +0300

    use Semaphore for tunnel conditions

--- src/ThreadsSecond/Stages/Tunnel.java
+++ src/ThreadsSecond/Stages/Tunnel.java
@@ -3,11 +3,12 @@ import ThreadsSecond.Vehicle.Car;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 import static ThreadsSecond.MainApp.CARS_COUNT;
 
 public class Tunnel extends Stage{
-    private static final CyclicBarrier tunnelBarrier = new CyclicBarrier(CARS_COUNT / 2);
+    private static final Semaphore smp = new Semaphore(CARS_COUNT/2);
     public Tunnel(){
         this.length = 80;
         this.description = "Тоннель " + length + " метров.";
@@ -16,12 +17,12 @@ public class Tunnel extends Stage{
     public void go(Car c) {
         try {
             System.out.println(c.getName() + " готовится к этапу: " + description);
-            tunnelBarrier.await();
+            smp.acquire();
             System.out.println(c.getName() + " начал этап: " + description);
             Thread.sleep(length / c.getSpeed() * 1000);
             System.out.println(c.getName() + " закончил этап: " + description);
-            tunnelBarrier.await();
-        } catch (BrokenBarrierException | InterruptedException e){
+            smp.release();
+        } catch (InterruptedException e){
             e.printStackTrace();
         }
     }
